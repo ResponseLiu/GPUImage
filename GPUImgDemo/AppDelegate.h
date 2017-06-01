8dd9a7dd2a7a8744e419a36c9b4625682571d539
Your Name <402663459@qq.com>
2017-06-01 15:26:00 +0800

    Initial Commit

---
+++ GPUImgDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPUImgDemo
+//
+//  Created by 1 on 2017/6/1.
+//  Copyright © 2017年 com.wh1.guozhentang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
