7dc94de0a25e9b48afa2ac5d093e676f2425a556
Your Name <402663459@qq.com>
2017-10-11 16:33:42 +0800

    first commit

---
+++ GPUImgDemo/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  GPUImgDemo
+//
+//  Created by 1 on 2017/10/11.
+//  Copyright © 2017年 com.wh1.guozhentang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
